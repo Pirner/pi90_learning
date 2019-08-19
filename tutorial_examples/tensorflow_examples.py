fb16cea6abb9b2a26534e1a39b47ff57996547fc
Stephan <stephan.pirner93@gmail.com>
2019-08-19 23:49:56 +0200

    first prototype of linear regression with tensorflow

--- tutorial_examples/tensorflow_examples.py
+++ tutorial_examples/tensorflow_examples.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class TfLinreg(object):
@@ -53,6 +54,11 @@ def train_linreg(sess, model, X_train, y_train, num_epochs=10):
     return training_costs
 
 
+def predict_linreg(sess, model, X_test):
+    y_pred = sess.run(model.z_net, feed_dict={model.X:X_test})
+
+    return y_pred
+
 X_train_test = np.arange(10).reshape((10, 1))
 y_train_test = np.array([1.0, 1.3, 3.1,
                     2.0, 5.0, 6.3,
@@ -64,3 +70,11 @@ sess = tf.Session(graph=lrmodel.g)
 training_costs = train_linreg(sess, lrmodel, X_train_test, y_train_test, 10)
 
 print(training_costs)
+
+plt.scatter(X_train_test, y_train_test, marker='s', s=50, label='Trainingsdaten')
+plt.plot(range(X_train_test.shape[0]), predict_linreg(sess, lrmodel, X_train_test), color='gray', marker='o', markersize=6, linewidth=3, label='LinReg-Model')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.tight_layout()
+plt.show()
