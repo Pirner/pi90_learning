aacf04a0f2899d78d404f6b5c62be4cf88f4d2ee
Stephan Pirner <SPirner@roedel.loc>
2019-08-21 15:59:20 +0200

    add keras example with custom data generator

---
+++ tutorial_examples/package_detection/DataIO/CustomDataGenerator.py
@@ -0,0 +1,95 @@
+import numpy as np
+import keras
+from tensorflow.python.keras.utils.data_utils import Sequence
+from PIL import Image
+from pudb import set_trace
+
+img_width = 3023
+img_height = 3023
+
+
+def preprocess_image(file):
+    pil_img = Image.open(file)
+    pil_img = pil_img.resize((img_width, img_height), Image.BILINEAR)
+    np_img = np.array(pil_img, dtype=np.uint16).reshape((img_width * img_height))
+    return np_img
+
+
+def preprocess_batch(x, y):
+    #n_files = len(x)
+    x_copy = []
+    for entry in x:
+        x_copy.append(preprocess_image(entry))
+    x_copy = np.array(x_copy)
+    y_copy = np.array(y)
+    print(x_copy.shape)
+    return x_copy, y_copy
+
+
+class DataGenerator(Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(32, 32, 32), n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def get_label_of_image(self, img_path):
+        i = 0
+        fail = -1
+        for path in self.list_IDs:
+            if path == img_path:
+                return i
+            i += 1
+        return fail
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, img_width*img_height))
+        y = np.empty(self.batch_size, dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            X[i, ] = preprocess_image(ID)
+
+            # Store class
+            index = self.get_label_of_image(ID)
+            if index >= 0:
+                #y[i] = np.array(self.labels[ID])
+                y[i] = self.labels[index]
+            else:
+                y[i] = 0
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+        #return X, y
---
+++ tutorial_examples/package_detection/ImageInfoV2.py
@@ -0,0 +1,126 @@
+import PIL.Image
+import numpy as np
+
+
+class ImageInfoV2:
+    # attributes
+    path = None
+    NFalsePositiveStructureComponents = 0
+    NFalsePositiveHeightWidthComponents = 0
+    # 3 is undefined package
+    PredictedPackageType = 3
+
+    AlgorithmDefinition = None
+    MeasuredWidth = 0
+    MeasuredHeight = 0
+    MeasuredSize = 0
+    Pitch = 0
+    LayerThickness = 0
+    CenterX = 0
+    CenterY = 0
+    # not implemented
+    Status = None
+    PlausiDecision = False
+    # not implemented
+    ImageStatus = None
+
+    Count = 0
+    TargetCount = 0
+    Accuracy = 0
+    PlausiCountCorrection = 0
+    ProcessSwitch = False
+    ExecTime = None
+    Seconds = 0
+    CenterHintX = 0
+    CenterHintY = 0
+    ReelName = None
+    ArticleName = None
+    ReelInnerRadius = None
+    MeasurementProbability = 0
+    PackageType = None
+    ImageNPArray = []
+    # attributes which are not from the ImageInfoV2
+    MachinePrecision = 0.00001
+    IsProcessed = False
+
+    def __init__(self):
+        pass
+
+    def readImage(self, path):
+        """This function reads the image from a path into a numpy array."""
+        image = PIL.Image.open(path)
+        pixel = np.array(image)
+        print(pixel.shape[0], pixel.shape[1])
+        pixel = pixel.reshape(1, pixel.shape[0] * pixel.shape[1])
+        # now reshape it for one line
+        # image = np.fromfile(path, )
+        self.ImageNPArray = pixel
+
+    def toString(self):
+        ret = ''
+        if self.NFalsePositiveStructureComponents is None:
+            ret = ret + 'NFalsePositiveStructureComponents: ' + 'None' + '\n'
+        else:
+            ret = ret + 'NFalsePositiveStructureComponents: ' + str(self.NFalsePositiveStructureComponents) + '\n'
+
+        if self.NFalsePositiveHeightWidthComponents is None:
+            ret = ret + 'NFalsePositiveHeightWidthComponents: ' + 'None' + '\n'
+        else:
+            ret = ret + 'NFalsePositiveHeightWidthComponents: ' + str(self.NFalsePositiveHeightWidthComponents) + '\n'
+
+        if self.PredictedPackageType is None:
+            ret = ret + 'PredictedPackageType: ' + 'None' + '\n'
+        else:
+            ret = ret + 'PredictedPackageType: ' + str(self.PredictedPackageType) + '\n'
+
+        if self.AlgorithmDefinition is None:
+            ret = ret + 'AlgorithmDefinition: ' + 'None' + '\n'
+        else:
+            ret = ret + 'AlgorithmDefinition: ' + self.AlgorithmDefinition + '\n'
+
+        if self.MeasuredWidth is None:
+            ret = ret + 'MeasuredWidth: ' + 'None' + '\n'
+        else:
+            ret = ret + 'MeasuredWidth: ' + str(self.MeasuredWidth) + '\n'
+
+        if self.MeasuredHeight is None:
+            ret = ret + 'MeasuredHeight: ' + 'None' + '\n'
+        else:
+            ret = ret + 'MeasuredHeight: ' + str(self.MeasuredHeight) + '\n'
+
+        if self.MeasuredSize is None:
+            ret = ret + 'MeasuredSize: ' + 'None' + '\n'
+        else:
+            ret = ret + 'MeasuredSize: ' + str(self.MeasuredSize) + '\n'
+
+        if self.Pitch is None:
+            ret = ret + 'Pitch: ' + 'None' + '\n'
+        else:
+            ret = ret + 'Pitch: ' + str(self.Pitch) + '\n'
+
+        if self.LayerThickness is None:
+            ret = ret + 'LayerThickness: ' + 'None' + '\n'
+        else:
+            ret = ret + 'LayerThickness: ' + str(self.LayerThickness) + '\n'
+
+        if self.CenterX is None:
+            ret = ret + 'CenterX: ' + 'None' + '\n'
+        else:
+            ret = ret + 'CenterX: ' + str(self.CenterX) + '\n'
+
+        if self.CenterY is None:
+            ret = ret + 'CenterY: ' + str(self.CenterY) + '\n'
+        else:
+            ret = ret + 'CenterY: ' + str(self.CenterY) + '\n'
+
+        if self.Status is None:
+            ret = ret + 'Status: ' + 'None' + '\n'
+        else:
+            ret = ret + 'Status: ' + self.Status + '\n'
+
+        if self.PlausiDecision is None:
+            ret = ret + 'PlausiDecision: ' + 'None' + '\n'
+        else:
+            ret = ret + 'PlausiDecision: ' + str(self.PlausiDecision) + '\n'
+
+        return ret
