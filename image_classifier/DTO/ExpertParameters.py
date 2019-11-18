4a431ae7738a4bf6f8b4d3a4f414f500705b137a
Stephan <stephan.pirner93@gmail.com>
2019-11-18 19:41:55 +0100

    cleanup

--- image_classifier/DTO/ExpertParameters.py
+++
@@ -1,7 +0,0 @@
-class ExpertParameters(object):
-    """This class is responsible for setting the expertSettings determined by the algorithm expert."""
-    _sampleData = 0
-
-    def __init__(self):
-        self._sampleData = 0
-
--- tut/clustering/playground.py
+++ tut/clustering/playground.py
@@ -12,18 +12,18 @@ X, y = make_blobs(
     random_state=0
 )
 
-plt.scatter(
-    X[:,0],
-    X[:,1],
-    c='white',
-    marker='o',
-    edgecolors='black',
-    s=50
-)
+#plt.scatter(
+#    X[:,0],
+#    X[:,1],
+#    c='white',
+#    marker='o',
+#    edgecolors='black',
+#    s=50
+#)
 
 km = KMeans(
     n_clusters=3,
-    n_init='random',
+    init='random',
     n_init=10,
     tol=1e-04,
     random_state=0
@@ -31,5 +31,46 @@ km = KMeans(
 
 y_km = km.fit_predict(X)
 
-# plt.grid()
-# plt.show()
+plt.scatter(
+    X[y_km==0,0],
+    X[y_km==0,1],
+    s=50,
+    c='lightgreen',
+    marker='s',
+    edgecolor='black',
+    label='Cluster 1'
+)
+
+plt.scatter(
+    X[y_km==1,0],
+    X[y_km==1,1],
+    s=50,
+    c='orange',
+    marker='o',
+    edgecolor='black',
+    label='Cluster 2'
+)
+
+plt.scatter(
+    X[y_km==2,0],
+    X[y_km==2,1],
+    s=50,
+    c='lightblue',
+    marker='v',
+    edgecolor='black',
+    label='Cluster 3'
+)
+
+plt.scatter(
+    km.cluster_centers_[:,0],
+    km.cluster_centers_[:,1],
+    s=250,
+    marker='*',
+    c='red',
+    edgecolor='black',
+    label='Zentroiden'
+)
+
+plt.legend(scatterpoints=1)
+plt.grid()
+plt.show()
